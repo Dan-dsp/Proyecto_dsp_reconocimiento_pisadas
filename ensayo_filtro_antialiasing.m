3e526278d4cb4b5a09cb106a5ebfaf8f0fa86d18
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2024-12-31 18:44:23 -0500

    Se están acomodando las funciones para su mejor organización. El filtro Butterworth aún está pendiente y requiere de un anásis en su estructura ya que se está comportando como un filtro paso banda.

--- ensayo_filtro_antialiasing.m
+++ ensayo_filtro_antialiasing.m
@@ -5,41 +5,8 @@
 % outputArg2 = inputArg2;
 % end
 
-clear;clc;
-%% read footstep trace ata
-
-% load data
-load People
-
-%%%%%%%%%%%%%%%%%%%%%% select a footstep trace to read %%%%%%%%%%%%%%%%%%%%
-% person_id: identifity of the person 
-%            choose from [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-person_id = 1;
-
-% sensor_num: the sensor number
-%             choose from [1,2,3,4,5]
-%             NOTE: data from some sensors could be empty due to sensory issues
-sensor_num = 1;
-
-% speed_scale: speed of walking represent the speed of mu, mu+sigma, mu+2*sigma, 
-%              mu+3*sigma, mu-sigma, mu-2*sigma, mu-3*sigma, and self-selected speed
-%              choose from [1, 2, 3, 4, 5, 6, 7, 8]
-%              NOTE: mu and sigma are average and standard deviations of
-%              step frequency
-speed_scale = 7;
-
-% amplification: the amplification factor 1) 6000x, 2) 4000x, 3) 2000x 
-%                choose from [1, 2, 3]
-amplification = 1;
-
-% trace_num: the footstep trace number, total number varies between 10 to 13
-%            choose from [1, 2, 3, ..., total number]
-trace_num = 1;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% read data
-data = people{person_id}.Sen{sensor_num}.S{speed_scale}...
-    {trace_num, amplification};
+% data = read_footstep_trace_data(1, 1, 7, 1, 1);
+data = read_footstep_trace_data(1, 1, 7, 1, 1);
 
 % % Parameters
 % fs = 1000;  % Sampling frequency (Hz)
---
+++ read_footstep_trace_data.m
@@ -0,0 +1,42 @@
+function data = read_footstep_trace_data(identification, sensor_number, speed_scalee, amplificationn, trace_number)
+%READ_FOOTSTEP_TRACE_DATA Summary of this function goes here
+
+% clear;clc;
+
+% read footstep trace ata
+% load data (remember to have your dataset's path already added in your worksapce)
+load People
+
+%%%%%%%%%%%%%%%%%%%%%% select a footstep trace to read %%%%%%%%%%%%%%%%%%%%
+% person_id: identifity of the person 
+%            choose from [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+person_id = 1;
+
+% sensor_num: the sensor number
+%             choose from [1,2,3,4,5]
+%             NOTE: data from some sensors could be empty due to sensory issues
+sensor_num = 1;
+
+% speed_scale: speed of walking represent the speed of mu, mu+sigma, mu+2*sigma, 
+%              mu+3*sigma, mu-sigma, mu-2*sigma, mu-3*sigma, and self-selected speed
+%              choose from [1, 2, 3, 4, 5, 6, 7, 8]
+%              NOTE: mu and sigma are average and standard deviations of
+%              step frequency
+speed_scale = 7;
+
+% amplification: the amplification factor 1) 6000x, 2) 4000x, 3) 2000x 
+%                choose from [1, 2, 3]
+amplification = 1;
+
+% trace_num: the footstep trace number, total number varies between 10 to 13
+%            choose from [1, 2, 3, ..., total number]
+trace_num = 1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% read data
+data = people{person_id}.Sen{sensor_num}.S{speed_scale}...
+    {trace_num, amplification};
+
+
+end
+
