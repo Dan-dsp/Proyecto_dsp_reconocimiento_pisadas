a629bf97a3d929b844b91fb9d87d4e171b85de45
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2025-01-01 16:37:22 -0500

    Cambios y adición de una carpeta para realizar el análisis de las señales obtenidas con ayuda del data inspector

--- processing_stage.m
+++ processing_stage.m
@@ -3,30 +3,35 @@
 data = read_footstep_trace_data(3, 1, 7, 1, 1);
 
 % % Parameters
-fs = 25600; % Sampling frequency (Hz)
+% fs = 25600; % Sampling frequency (Hz)
+fs = 10000; % Sampling frequency (Hz)
 fc = 200; % Cutoff frequency (Hz)
 time = 0 : 1/fs : (1/fs) * (length(data) - 1); % Time vector for 1 second of data
 
 % Call the antialiasing filter function
 filtered_data = antialiasing_filter(data, fs, fc);
 
-% Plots the frequency's domain plot of the data signal
-frequency_domain(data, fs)
-% Plots the frequency's domain plot of the filtered data signal
-frequency_domain(filtered_data, fs)
+% % Plots the frequency's domain plot of the data signal
+% frequency_domain(data, fs)
+% % Plots the frequency's domain plot of the filtered data signal
+% frequency_domain(filtered_data, fs)
+% 
+% % Plot results
+% figure;
+% subplot(2, 1, 1);
+% plot(time, data);
+% title('Raw Data');
+% xlabel('Time (s)');
+% ylabel('Amplitude');
+% 
+% subplot(2, 1, 2);
+% plot(time, filtered_data);
+% title('Filtered Data (Antialiasing Applied)');
+% xlabel('Time (s)');
+% ylabel('Amplitude');
 
-% Plot results
-figure;
-subplot(2, 1, 1);
-plot(time, data);
-title('Raw Data');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-subplot(2, 1, 2);
-plot(time, filtered_data);
-title('Filtered Data (Antialiasing Applied)');
-xlabel('Time (s)');
-ylabel('Amplitude');
+Simulink.sdi.clear;
+Simulink.sdi.plot(data, time);
+Simulink.sdi.plot(filtered_data, time);
 
 
