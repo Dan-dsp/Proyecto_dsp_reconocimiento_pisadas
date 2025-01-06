7dd29fc5cb60e2dc66d0098839478442cc662388
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2025-01-06 11:29:16 -0500

    Análisis para la implementación de la transformada Wavelet completado y resumido con la creación de la función "wavelet_analysis" llamada en el archivo "wavelet_filter"

---
+++ wavelet_analysis.m
@@ -0,0 +1,40 @@
+function wavelet_analysis(signal, fs, min_frequency_limit, max_frequency_limit)
+
+    % Spectrogram Parameters
+    window_size = 256;       % Window size (adjust as needed)
+    overlap = round(0.8 * window_size); % 80% overlap
+    nfft = 1024;             % Number of FFT points
+
+    % Spectrogram
+    figure;
+    spectrogram(signal, window_size, overlap, nfft, fs, 'yaxis');
+    title('Spectrogram of the Signal');
+    xlabel('Time (s)');
+    ylabel('Frequency (Hz)');
+    colorbar;
+    
+    % Wavelet Transform
+    figure;
+    cwt(signal, 'amor', fs); % 'amor' uses Morlet wavelet, adjust as needed
+    title('Magnitude Scalogram of the Signal');
+    colorbar;
+    
+    % Wavelet analysis
+    fb = cwtfilterbank('SignalLength', length(signal), 'SamplingFrequency', fs, ...
+                       'FrequencyLimits', [min_frequency_limit max_frequency_limit]); % Focus on 20-90 Hz
+
+    % Compute the scalogram
+    [cfs, frequencies] = wt(fb, signal);
+
+    % Plot the magnitude scalogram
+    figure;
+    t = (0:length(signal)-1) / fs; % Time vector
+    imagesc(t, frequencies, abs(cfs)); % Magnitude of coefficients
+    axis xy;
+    title('Magnitude Scalogram of the Signal');
+    xlabel('Time (s)');
+    ylabel('Frequency (Hz)');
+    colorbar;
+
+end
+
