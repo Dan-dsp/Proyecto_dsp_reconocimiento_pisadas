72ec63a778daffcde50f6978d5f91b6d97c9dda6
Dan-dsp <daniel.pelaez@correounivalle.edu.co>
2025-01-06 18:27:37 -0500

    En signal_plot está lo necesario para graficar un gráfico normal y un escalograma de la señal sin filtrar

--- signal_plot.m
+++ signal_plot.m
@@ -1,12 +1,13 @@
-function signal_plot(signal, titulo, x_label, y_label)
-%SIGNAL_PLOT Summary of this function goes here
-%   Detailed explanation goes here
-    time = length(signal);
-    figure;
-    plot(time, signal);
-    title(titulo);
-    xlabel(x_label);
-    ylabel(y_label);
-    grid on;
-end
+signal = read_footstep_trace_data(1, 1, 1, 1, 1);
+% 
+% function signal_plot(signal, 'señal', 'tiempo', 'amplitud');
+    % time = length(signal);
+    % figure;
+    % plot(time, signal);
+    % title('señal');
+    % xlabel('tiempo');
+    % ylabel('amplitud');
+    % grid on;
+% end
 
+scalogram(signal, 1000);
